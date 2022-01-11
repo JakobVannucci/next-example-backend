d9e96eafeaa14e759da474d5482e58fd4186b8e1
Jakob Vannucci <jakob.vannucci@protestsoftware.com.au>
2022-01-12 08:42:53 +1000

    Add in the user controller

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+    Route::get('/user', [UserController::class, 'show']);
 });
